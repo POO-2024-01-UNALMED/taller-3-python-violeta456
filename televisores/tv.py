f79e14fc90089a470ed65c8d6db16a1a88d5805c
violeta456 <159485730+violeta456@users.noreply.github.com>
2024-02-24 11:05:51 -0500

    terminado

---
+++ televisores/tv.py
@@ -0,0 +1,70 @@
+class tv:
+    _numTV=0
+
+    def __init__(self, marca,estado):
+        self._marca=marca
+        self._canal=1
+        self._precio=500
+        self._estado=estado
+        self._volumen=1
+        self._control
+        tv._numTV+=1
+
+    def setMarca(self,marca):
+        self._marca=marca
+    def getMarca(self):
+        return self._marca
+    
+    def setCanal(self,canal):
+        if (canal>0 and canal<121 and self._estado==True):
+            self._canal=canal
+    def getCanal(self):
+        return self._canal
+    
+    def setPrecio(self,precio):
+        self._precio=precio
+    def getPrecio(self):
+        return self._precio
+    
+    def setVolumen(self,volumen):
+        if (volumen>-1 and volumen<8 and self._estado==True):
+            self._volumen=volumen
+    def getVolumen(self):
+        return self._volumen
+    
+    def setControl(self,control):
+        self._control=control
+    def getControl(self):
+        return self._control
+    
+
+    @classmethod
+    def setNumTV(cls,numTV):
+        cls._numTV = numTV
+    def getNumTV(cls):
+        return cls._numTV
+    
+    def turnOn(self):
+        self._estado=True
+    def turnOff(self):
+        self._estado=False
+    def getEstado(self):
+        return self._estado
+    
+    def canalUp(self):
+        if (self._canal<121 and self._estado==True):
+            self._canal+=1
+    def canalDown(self):
+        if (self._canal>1 and self._estado==True):
+            self._canal-=1
+    
+    def volumenUp(self):
+        if (self._volumen<7 and self._estado==True):
+            self._volumen+=1
+    def volumenDown(self):
+        if (self._volumen>0 and self._estado==True):
+            self._volumen-=1
+    
+
+
+    

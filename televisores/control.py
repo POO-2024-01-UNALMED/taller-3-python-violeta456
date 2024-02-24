366ecf84b5a24df815fbbb3351c983d00dd37bd4
violeta456 <159485730+violeta456@users.noreply.github.com>
2024-02-24 11:43:08 -0500

    setCanal y setVolumen

--- televisores/control.py
+++ televisores/control.py
@@ -14,10 +14,10 @@ class Control:
         self._tv.volumenUp()
     def volumenDown(self):
         self._tv.volumenDown()
-    def setCanal(self):
-        self._tv.setCanal()
-    def setVolumen(self):
-        self.tv.setVolumen()
+    def setCanal(self,canal):
+        self._tv.setCanal(canal)
+    def setVolumen(self, volumen):
+        self.tv.setVolumen(volumen)
     
     def enlazar(self, tv):
         self._tv=tv
